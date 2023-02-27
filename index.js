19068562d2e93fcc5b18e5d4130a161f04b9eb6a
Adrian Goding <godingadrian@gmail.com>
2023-02-26 19:08:14 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
